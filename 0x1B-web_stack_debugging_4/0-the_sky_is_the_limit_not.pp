e1b3f009ede168795d0e6b52c06710786c0e0c59
Makoena20 <koenaletseku@gmail.com>
2024-06-28 06:04:15 +0000

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,19 @@
-# This Puppet manifest optimizes Nginx configuration for handling high concurrency and large number of requests
-exec { 'fix--for-nginx':
-  command => '/bin/sed -i "s/worker_connections [0-9]*;/worker_connections 4096;/" /etc/nginx/nginx.conf && /bin/sed -i "s/keepalive_timeout [0-9]*;/keepalive_timeout 65;/" /etc/nginx/nginx.conf && /bin/sed -i "s/# multi_accept on;/multi_accept on;/" /etc/nginx/nginx.conf && /bin/sed -i "s/# server_tokens off;/server_tokens off;/" /etc/nginx/nginx.conf && service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+# Puppet manifest to optimize Nginx for handling high concurrency requests
+
+class nginx_optimization {
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    content => template('nginx/nginx.conf.erb'),
+    notify  => Service['nginx'],
+  }
+
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    require    => File['/etc/nginx/nginx.conf'],
+  }
 }
+
+include nginx_optimization
 
