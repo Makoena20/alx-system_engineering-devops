689b3c1601588b9deada5fd64717e87f79778c5f
Makoena20 <koenaletseku@gmail.com>
2024-07-04 01:07:52 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,25 +3,31 @@
 This module contains the function number_of_subscribers.
 """
 
+import sys
 import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
+    elif response.status_code == 404:
+        print(f"Subreddit '{subreddit}' not found.")
+        return 0
     else:
+        print(f"Error fetching data for subreddit '{subreddit}'. Status code: {response.status_code}")
         return 0
 
 if __name__ == '__main__':
-    import sys
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
         subreddit = sys.argv[1]
         print(number_of_subscribers(subreddit))
-
 
